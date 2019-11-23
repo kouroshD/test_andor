698eb2b71c0ebb4a06d04777c7ea1be18c07208f
Kourosh <kourosh.darvish@gmail.com>
2019-11-24 00:29:31 +0100

    initial commit, started from previous impelemntation in ANDOR/hierarchical branch, dependencies are fixed

---
+++ src/test_andor.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <ros/ros.h>
+#include "std_msgs/String.h"
+#include <andor/andorSRV.h>
+#include <andor/Hyperarc.h>
+#include <andor/Node.h>
+
+using namespace std;
+int main(int argc, char **argv)
+{
+	string andorGraphName;
+	bool isGraphSolved;
+
+	ros::init(argc, argv, "test_andor");
+	ros::NodeHandle nh;
+	ros::ServiceClient andorSRV_client = nh.serviceClient<andor::andorSRV>("andorService");
+
+	cout<<"- Insert AND/OR graph name: ";
+	cin>>andorGraphName;
+
+	andor::andorSRV andor_srv0 ;
+	andor_srv0.request.graphName=andorGraphName;
+
+	if (andorSRV_client.call(andor_srv0))
+	{
+		isGraphSolved=andor_srv0.response.graphSolved;
+		cout<<endl<<"+ is the graph solved:"<<isGraphSolved<<endl;
+
+		cout<<"Feasible Nodes:"<<endl;
+		for (int i=0;i<andor_srv0.response.feasibleNodes.size();i++)
+		{
+			string feasbible_state_name=andor_srv0.response.feasibleNodes[i].nodeName;
+			string andorNameHierarchy=andor_srv0.response.feasibleNodes[i].graphName;
+			int cost=andor_srv0.response.feasibleNodes[i].nodeCost;
+
+			cout<<i+1<<") "<<andorNameHierarchy<<" , "<<feasbible_state_name<<" , "<<cost<<endl;
+		}
+		cout<<endl<<"Feasible Hyper-arcs:"<<endl;
+		for (int i=0;i<andor_srv0.response.feasibleHyperarcs.size();i++)
+		{
+			string feasbible_state_name=andor_srv0.response.feasibleHyperarcs[i].hyperarcName;
+			string andorNameHierarchy=andor_srv0.response.feasibleHyperarcs[i].graphName;
+			int cost=andor_srv0.response.feasibleHyperarcs[i].hyperarcCost;
+
+			cout<<i+1<<") "<<andorNameHierarchy<<" , "<<feasbible_state_name<<" , "<<cost<<endl;
+		}
+	}
+
+	cout<<"********************"<<endl;
+	while(ros::ok())
+	{
+		andor::andorSRV andor_srv;
+		andor_srv.request.graphName=andorGraphName;
+
+		string stateName, type, graphName;
+		cout<<"insert following info for solved state/state transition: [N(Node)/H(Hyper-arc)] [state name] [graph name]"<<endl;
+		cin>>type>>stateName>>graphName;
+		bool call_srv=false;
+		if(type=="N" || type=="n")
+		{
+			call_srv=true;
+			andor::Node solvedNode;
+			solvedNode.nodeName=stateName;
+			solvedNode.graphName=graphName;
+			andor_srv.request.solvedNodes.push_back(solvedNode);
+		}
+		else if(type=="H" || type=="h")
+		{
+			call_srv=true;
+			andor::Hyperarc solvedHA;
+			solvedHA.hyperarcName=stateName;
+			solvedHA.graphName=graphName;
+			andor_srv.request.solvedHyperarc.push_back(solvedHA);
+		}
+		else
+		{
+			cout<<"wrtitten type is wrong, inser again! "<<type<<endl;
+		}
+
+		if(call_srv==true)
+		{
+			if (andorSRV_client.call(andor_srv))
+			{
+				isGraphSolved=andor_srv.response.graphSolved;
+				cout<<endl<<"+ is the graph solved:"<<isGraphSolved<<endl;
+
+				cout<<"Feasible Nodes:"<<endl;
+				for (int i=0;i<andor_srv.response.feasibleNodes.size();i++)
+				{
+					string feasbible_state_name=andor_srv.response.feasibleNodes[i].nodeName;
+					string andorNameHierarchy=andor_srv.response.feasibleNodes[i].graphName;
+					int cost=andor_srv.response.feasibleNodes[i].nodeCost;
+
+					cout<<i+1<<") "<<andorNameHierarchy<<" , "<<feasbible_state_name<<" , "<<cost<<endl;
+				}
+				cout<<endl<<"Feasible Hyper-arcs:"<<endl;
+				for (int i=0;i<andor_srv.response.feasibleHyperarcs.size();i++)
+				{
+					string feasbible_state_name=andor_srv.response.feasibleHyperarcs[i].hyperarcName;
+					string andorNameHierarchy=andor_srv.response.feasibleHyperarcs[i].graphName;
+					int cost=andor_srv.response.feasibleHyperarcs[i].hyperarcCost;
+
+					cout<<i+1<<") "<<andorNameHierarchy<<" , "<<feasbible_state_name<<" , "<<cost<<endl;
+				}
+			}
+		}
+
+
+		cout<<"********************"<<endl;
+	}
+
+
+	return 1;
+}
